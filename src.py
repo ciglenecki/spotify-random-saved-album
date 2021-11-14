dd5dd16fb5035797afc9f0b8974cfd58506d0734
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2021-11-14 19:07:28 +0100

    update code

--- src.py
+++ src.py
@@ -1,22 +1,27 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import random
 import os
+import spotipy
 from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 ID = os.getenv("ID")
 SEC = os.getenv("SEC")
 
-
 sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(client_id=ID, client_secret=SEC, redirect_uri="http://127.0.0.1:9090", scope="user-library-read")
+    auth_manager=SpotifyOAuth(
+        client_id=ID,
+        client_secret=SEC,
+        redirect_uri="http://127.0.0.1:9090",
+        scope="user-library-read",
+    )
 )
 
-results = sp.current_user_saved_albums()
-random_num = random.randint(0, len(results["items"]) - 1)
-random_album = results["items"][random_num]["album"]
-name = random_album["name"]
-url = random_album["external_urls"]["spotify"]
+albums = sp.current_user_saved_albums()
+random_index = random.randint(0, len(albums["items"]) - 1)
+album = albums["items"][random_index]["album"]
+album_name = album["name"]
+album_url = album["external_urls"]["spotify"]
 
-print(url)
+# print(album_name, album_url) # If you want to print album's name as well
+print(album_url)
